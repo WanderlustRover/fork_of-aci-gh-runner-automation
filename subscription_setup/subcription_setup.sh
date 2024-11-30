c73c7684cf653ef97f57e28905048e377f00eea5
WanderlustRover <11522872+WanderlustRover@users.noreply.github.com>
2024-11-30 16:18:24 -0500

    test1

--- subscription_setup/subcription_setup.sh
+++ subscription_setup/subcription_setup.sh
@@ -5,7 +5,6 @@
     # Storage Account and Container to store Terraform State remotely.
     # Creates a Service Principal and then assigns contributor at tenant root. Note: you may wish to reduce this scope for your deployment down to single Subscriptions etc!
     # Please change the variables to suit your requirements!
-    # Test1
  
 az login
 
